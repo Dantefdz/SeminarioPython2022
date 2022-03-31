8889ba98147ca5e36846fbfc69f58f3bb1546d72
Dantefdz <88099408+Dantefdz@users.noreply.github.com>
2022-03-30 22:03:23 -0300

    5 bien hecho

--- practica2/ejer5.py
+++ practica2/ejer5.py
@@ -1,6 +1,6 @@
 from collections import Counter
 
 frase=input('ingrese la frase:')
-palabra=input('ingrese la palabra')
+palabra=input('ingrese la palabra:')
 print(Counter(frase.split())[palabra])
 
